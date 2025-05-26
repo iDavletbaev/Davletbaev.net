f7142c756796421c2d7164b9e1a5aa630d6bf678
Ilnur Davletbaev <ilnur@davletbaev.net>
2025-05-26 22:45:37 +0500

    Доработка первого блока на главной. Выделил кастомный курсор в отдельный файл и отключил его для админов

--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -1,4 +1,5 @@
-<?php if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
 use Bitrix\Main\Page\Asset;
 
 global $USER;
@@ -17,42 +18,31 @@ global $USER;
     Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/css/style.css");
     Asset::getInstance()->addCss("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css");
     ?>
-
-    <?php
-    if ($USER->IsAdmin()) { ?>
-    <style>
-        .header {
-            position: unset!important;
-        }
-        </style>
-    <?php } ?>
-
 </head>
 <body>
 
-<!-- Шапка с меню -->
 <header class="header">
-    <?php $APPLICATION->ShowPanel();?>
+    <?php $APPLICATION->ShowPanel(); ?>
     <div class="container">
         <nav class="navbar">
-            <a href="#" class="logo">Ilnur <span>Davletbaev</span></a>
+            <a href="/" class="logo">Ilnur <span>Davletbaev</span></a>
 
-            <?$APPLICATION->IncludeComponent("bitrix:menu", "top", Array(
-	"ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
-		"CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
-		"DELAY" => "N",	// Откладывать выполнение шаблона меню
-		"MAX_LEVEL" => "1",	// Уровень вложенности меню
-		"MENU_CACHE_GET_VARS" => array(	// Значимые переменные запроса
-			0 => "",
-		),
-		"MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
-		"MENU_CACHE_TYPE" => "A",	// Тип кеширования
-		"MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
-		"ROOT_MENU_TYPE" => "top",	// Тип меню для первого уровня
-		"USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
-	),
-	false
-);?>
+            <? $APPLICATION->IncludeComponent("bitrix:menu", "top", array(
+                "ALLOW_MULTI_SELECT" => "N",    // Разрешить несколько активных пунктов одновременно
+                "CHILD_MENU_TYPE" => "left",    // Тип меню для остальных уровней
+                "DELAY" => "N",    // Откладывать выполнение шаблона меню
+                "MAX_LEVEL" => "1",    // Уровень вложенности меню
+                "MENU_CACHE_GET_VARS" => array(    // Значимые переменные запроса
+                    0 => "",
+                ),
+                "MENU_CACHE_TIME" => "3600",    // Время кеширования (сек.)
+                "MENU_CACHE_TYPE" => "A",    // Тип кеширования
+                "MENU_CACHE_USE_GROUPS" => "Y",    // Учитывать права доступа
+                "ROOT_MENU_TYPE" => "top",    // Тип меню для первого уровня
+                "USE_EXT" => "N",    // Подключать файлы с именами вида .тип_меню.menu_ext.php
+            ),
+                false
+            ); ?>
 
             <!-- Кнопка бургер для мобильных -->
             <!--<div class="hamburger">

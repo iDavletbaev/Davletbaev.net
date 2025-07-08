d827fd7614847a93bdfb01a7b6855f370e89f9e2
Ilnur Davletbaev <ilnur@davletbaev.net>
2025-07-08 21:57:07 +0500

    Core

---
+++ bitrix/php_interface/after_connect_d7.php
@@ -0,0 +1,4 @@
+<?php
+$this->queryExecute("SET NAMES 'utf8mb4'");
+$this->queryExecute("SET collation_connection = 'utf8mb4_0900_ai_ci'");
+$this->queryExecute("SET LOCAL time_zone='".date('P')."'");

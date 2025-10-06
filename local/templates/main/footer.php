241a0b23a632fbbf20debec13d61ac4ecd9e0e16
Ilnur Davletbaev <ilnur@davletbaev.net>
2025-10-06 15:09:06 +0500

    Меню вывод сквозных данных в подвале

--- local/templates/main/footer.php
+++ local/templates/main/footer.php
@@ -5,8 +5,6 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 use Bitrix\Main\Page\Asset;
 
 Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/main.js");
-
-d($GLOBALS['CONTACTS']);
 ?>
 
 <!-- Подвал -->
